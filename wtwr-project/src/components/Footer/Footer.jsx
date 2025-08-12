5eeb135a61ee583ca96961dc97f369b0577179e3
DillonRosenberger <drosey15@gmail.com>
2025-08-11 20:43:37 -0700

    Add style to cards and add footer

---
+++ wtwr-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__name">Developed by Dillon Rosenberger</p>
+      <p className="footer__year">2025</p>
+    </div>
+  );
+}
+
+export default Footer;
--- wtwr-project/src/components/Main/Main.jsx
+++ wtwr-project/src/components/Main/Main.jsx
@@ -3,18 +3,22 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
 
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
-      <section className="main-cards">
-        <p className="main-cards__text">
+      <section className="cards">
+        <p className="cards__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
-        <ul className="main-cards__list">
-          {defaultClothingItems.map((item) => {
-            return <ItemCard key={item._id} item={item} />;
-          })}
+        <ul className="cards__list">
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
         </ul>
       </section>
     </main>
