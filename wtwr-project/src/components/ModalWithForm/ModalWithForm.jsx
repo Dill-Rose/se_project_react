203c7f22fbf31c47e7ebdd91a2b7d4d2e1bcbcce
DillonRosenberger <drosey15@gmail.com>
2025-08-12 21:03:22 -0700

    Add modalWithForm

---
+++ wtwr-project/src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,23 @@
+import "./modalwithform.css";
+import close from "../../assets/close.png";
+
+function ModalWithForm({ children, buttonText, title }) {
+  return (
+    <div className="modal">
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button type="button" className="modal__close">
+          <img src={close} alt="close" />
+        </button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
